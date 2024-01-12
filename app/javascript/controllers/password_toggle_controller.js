787b406b38f8d0b1eed98682671029855a7e22cd
sergiohc <sergioh.c@live.com>
2024-01-12 17:25:53 -0300

    add feature to toggle password

--- app/javascript/controllers/password_toggle_controller.js
+++ app/javascript/controllers/password_toggle_controller.js
@@ -1,18 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="password-toggle"
 export default class extends Controller {
+  static targets = ["input"]
 
-  static targets = ["unhide"]
   password(e) {
-    console.log("password toggle")
-    if (this.input.type === "password") {
-      e.target.classList.remove('bi bi-eye-slash');
-      e.target.classList.add('bi bi-eye');
-      this.input.type = "text";
+    if (this.inputTarget.type === "password") {
+      e.target.classList.remove('bi', 'bi-eye-slash');
+      e.target.classList.add('bi', 'bi-eye');
+      this.inputTarget.type = "text";
     } else {
-      e.target.classList.remove('bi bi-eye');
-      e.target.classList.add('bi bi-eye-slash');
-      this.input.type = "password";
+      e.target.classList.remove('bi', 'bi-eye');
+      e.target.classList.add('bi', 'bi-eye-slash');
+      this.inputTarget.type = "password";
     }
   }
+
 }
